6c4c075d4517163a70b7bbf55b5eae1955f22aea
Justin Hawkins <justin@hawkins.id.au>
2012-08-06 20:31:21 +0930

    First commit of bundle.

---
+++ lib/Bundle/Justin.pm
@@ -0,0 +1,31 @@
+package Bundle::Justin;
+
+$VERSION = "0.01";
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Justin - A bundle to install the stuff I like from CPAN.
+
+=head1 SYNOPSIS
+
+  perl -MCPAN -e 'install Bundle::Justin'
+
+=head1 CONTENTS
+
+  DBI
+  DBD::Pg
+
+  DateTime
+
+  Mojolicious
+
+=head1 AUTHOR
+
+Justin Hawkins E<lt>justin@cpan.orgE<gt>
+
+=cut 
+
